9eb98f6656288d67bbe9fb569a7eb2d7787be0d2
Pranav Vaish <pranav.vaish@gmail.com>
2014-05-23 17:23:31 +0530

    Game able to detect the turn of the person with properly assigning Xs and Os

--- main-code-vaish.py
+++ main-code-vaish.py
@@ -21,18 +21,36 @@ def check_winner(matrix):
     else:
         return 's'
     
-matrix =10*[['s','s','s'],['s','s','s'],['s','s','s']]
-bg="bg.jpg"
-crimg="x.png"
-oimg="o.png"
+matrix=[[[['s','s','s'],['s','s','s'],['s','s','s']] for x in xrange(3)] for x in xrange(3)]
+main_matrix=[['s','s','s'],['s','s','s'],['s','s','s']]
+
+coord_of_cells = [[[[[0 for x in xrange(2)] for x in xrange(3)] for x in xrange(3)] for x in xrange(3)] for x in xrange(3)]
+
+for x in xrange(3):
+    for y in xrange(3):
+        for i in xrange(3):
+            for j in xrange(3):
+                coord_of_cells[x][y][i][j]=(210*y+70*j,210*x+70*i)
+                
+bg="./img/grid.png"
+xsimg="./img/xsmall.png"
+osimg="./img/osmall.png"
+xlimg="./img/xbig.png"
+olimg="./img/obig.png"
 
 pygame.init()
+    
+screen=pygame.display.set_mode((630,630),0,32)
 
 BackgroundimageObject=pygame.image.load(bg).convert()
-XimageObject=pygame.image.load(crimg).convert_alpha()
-OimageObject=pygame.image.load(oimg).convert_alpha()
-    
-screen=pygame.display.set_mode((600,600),0,32)
+XSMALLimageObject=pygame.image.load(xsimg).convert_alpha()
+OSMALLimageObject=pygame.image.load(osimg).convert_alpha()
+XLARGEimageObject=pygame.image.load(xlimg).convert_alpha()
+OLARGEimageObject=pygame.image.load(olimg).convert_alpha()
+
+player=1
+
+screen.blit(BackgroundimageObject,(0,0))
 
 while True:
 
@@ -41,9 +59,26 @@ while True:
             pygame.quit()
             sys.exit()
 
-        if event.type==MOUSEDOWN:
-            pass
+        if event.type==MOUSEBUTTONDOWN:
+            pos=pygame.mouse.get_pos()
+            for x in xrange(3):
+                for y in xrange(3):
+                    for i in xrange(3):
+                        for j in xrange(3):
+                            if ((pos[0]>coord_of_cells[x][y][i][j][0])and(pos[0]<coord_of_cells[x][y][i][j][0]+70)and(pos[1]>coord_of_cells[x][y][i][j][1])and(pos[1]<coord_of_cells[x][y][i][j][1]+70)):
+                                if (matrix[x][y][i][j]!='s'):
+                                    pass
+                                else:
+                                    if(player%2==1):
+                                        screen.blit(XSMALLimageObject,coord_of_cells[x][y][i][j])
+                                        player+=1
+                                        matrix[x][y][i][j]='x'
 
-    screen.blit(background,(0,0))
+                                    else:
+                                        screen.blit(OSMALLimageObject,coord_of_cells[x][y][i][j])
+                                        player+=1
+                                        matrix[x][y][i][j]='o'
+
+                                    
 
     pygame.display.update()
