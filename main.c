aa7d151ff0083e58c6e0515d23ed4c6447a99d1a
Anand Singh Kunwar <anandkunwar95@gmail.com>
2014-06-05 21:55:23 +0530

    Added check functions

--- main.c
+++ main.c
@@ -7,7 +7,94 @@ struct Node
 int minimax();
 int minmove();
 int maxmove();
+char check_winner(char [][3]);
+char check_win_main(char [][3]);
 
+char check_winner(char matrix[][3])
+{
+        if(matrix[0][0]==matrix[0][1]&&matrix[0][1]==matrix[0][2]&&matrix[0][2]!='s')
+                return matrix[0][0];
+        if(matrix[1][0]==matrix[1][1]&&matrix[1][1]==matrix[1][2]&&matrix[1][2]!='s')
+                return matrix[1][0];
+        if(matrix[2][0]==matrix[2][1]&&matrix[2][1]==matrix[2][2]&&matrix[2][2]!='s')
+                return matrix[2][0];
+
+        if(matrix[0][0]==matrix[1][0]&&matrix[1][0]==matrix[2][0]&&matrix[2][0]!='s')
+                return matrix[0][0];
+        if(matrix[0][1]==matrix[1][1]&&matrix[1][1]==matrix[2][1]&&matrix[2][1]!='s')
+                return matrix[0][1];
+        if(matrix[0][2]==matrix[1][2]&&matrix[1][2]==matrix[2][2]&&matrix[2][2]!='s')
+                return matrix[0][2];
+
+        if(matrix[0][0]==matrix[1][1]&&matrix[1][1]==matrix[2][2]&&matrix[2][2]!='s')
+                return matrix[0][0];
+        if(matrix[0][2]==matrix[1][1]&&matrix[1][1]==matrix[2][0]&&matrix[2][0]!='s')
+                return matrix[0][2];
+
+        int x,y;
+        for(x=0;x<3;x++)
+        {
+                for(y=0;y<3;y++)
+                {
+                        if(matrix[x][y]=='s')
+                                return 's';
+                }
+        }
+
+        return 't';
+}
+
+char check_win_main(char main_matrix[][3])
+{
+        char copy_main[3][3];
+
+        int x,y;
+        for(x=0;x<3;x++)
+        {
+                for(y=0;y<3;y++)
+                {
+                        if(main_matrix[x][y]=='t')
+                        {
+                                copy_main[x][y]='s';
+                        }
+
+                        else
+                        {
+                                copy_main[x][y]=main_matrix[x][y];
+                        }
+                }
+        }
+
+        if(check_winner(copy_main)=='o')
+        {
+                return 'o';
+        }
+        if(check_winner(copy_main)=='x')
+        {
+                return 'x';
+        }
+
+        int flag=0;
+
+        for(x=0;x<3;x++)
+        {
+                for(y=0;y<3;y++)
+                {
+                        if(main_matrix[x][y]=='s')
+                        {
+                                flag=1;
+                                break;
+                        }
+                }
+        }
+
+        if(flag==0)
+        {
+                return 't';
+        }
+
+        return 's';
+}
 
 int minmove()
 	{
