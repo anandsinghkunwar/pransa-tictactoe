56c836abe16a97a57c62f95e5a427b2b3ff378f4
Anand Singh Kunwar <anandkunwar95@gmail.com>
2014-06-05 17:38:45 +0530

    Added Node, minmove, maxmove

--- main.c
+++ main.c
@@ -1,7 +1,46 @@
 #include<stdio.h>
+struct Node
+	{
+		int val;
+		struct Node * child[81];
+	}root, present, next, temp;
+int minimax();
+int minmove();
+int maxmove();
 
+
+int minmove()
+	{
+		int i,min,move;
+		for(i=0;i<81;i++)
+			{
+				next=present->child[i];
+				if (next->val<min)
+					{
+						min=next->val;
+						move=i;
+					}
+			}
+		return move;
+	}
+
+int maxmove()
+        {
+                int i,max,move;
+                for(i=0;i<81;i++)
+                        {
+                                next=present->child[i];
+                                if (next->val>max)
+                                        {
+                                                max=next->val;
+                                                move=i;
+                                        }
+                        }
+                return move;
+        }
 int main()
 	{
+		int a[3][3][3][3];
 		printf("Trial");
 		return 0;
 	}
