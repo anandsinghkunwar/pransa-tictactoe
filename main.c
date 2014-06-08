0ead2a3860c203e2354d82e669924af262faa90b
Anand Singh Kunwar <anandkunwar95@gmail.com>
2014-06-08 18:31:09 +0530

    Minimax Function made

--- main.c
+++ main.c
@@ -5,15 +5,15 @@ struct Node
 	{
 		int val;
 		struct Node * child[3][3][3][3];
-	}*root, *present, *next, *temp;
+	};
 int minimax(struct Node *,char,char [3][3][3][3]);
-int minmove();
-int maxmove();
+int minmove(struct Node *);
+int maxmove(struct Node *);
 char check_winner(char [][3]);
 char check_win_main(char [][3]);
 int * check_empty(char [3][3][3][3], int, int);
 
-int * check_empty(char matrix[3][3][3][3], int a, int b)
+int * check_empty(char matrix[3][3][3][3],char main_matrix[3][3], int a, int b)
         {
                 int i,j,k,l,count=0;
                 int * moves = (int *) malloc(sizeof(int)*81);
@@ -28,7 +28,7 @@ int * check_empty(char matrix[3][3][3][3], int a, int b)
                                                         for(l=0;l<3;i++)
                                                                 {
 
-                                                                        if(matrix[i][j][k][l]=='s')
+                                                                        if(matrix[i][j][k][l]=='s'&&main_matrix=='s')
                                                                                 {
                                                                                         moves[count]=i*1000+j*100+k*10+l;
                                                                                         count++;
@@ -142,29 +142,10 @@ char check_win_main(char main_matrix[][3])
         return 's';
 }
 
-int minmove(char matrix[3][3][3][3])
+int minmove(struct Node * present)
         {
-                int i,j,k,l,min=32767,move;
-                for(i=0;i<3;i++)
-                        {
-                        for(j=0;j<3;i++)
-                                {
-                                        for(k=0;k<3;i++)
-                                                {
-                                                        for(l=0;l<3;i++)
-                                                                {
-                                                                        if(matrix[i][j][k][l]!='s')
-                                                                                continue;
-                                                                        next=present->child[i][j][k][l];
-                                                                        if (next->val<min)
-                                                                                {
-                                                                                        min=next->val;
-                                                                                        move=i*1000+j*100+k*10+l;
-                                                                                }
-                                                                }
-                                                }
-                                }
-                        }
+		int move,i;
+		for(i=0;empty[i]!=-1&&i<81;i++)
                 return move;
         }
 
@@ -195,52 +176,98 @@ int maxmove(char matrix[3][3][3][3])
                         }
                 return move;
         }
-int minimax(struct Node *current,char turn,char matrix[3][3][3][3])
+int minimax(struct Node *current,char turn,char matrix[3][3][3][3],char main_matrix[3][3],char x,char y)
 	{
-		int i,*empty;
+		int i,*empty,a,b,c,d;
 		char copy[3][3][3][3];
 		for(i=0;i<81;i++)
 			current->child[i]=NULL;
-		if (checkwin()=='x')
+		if (check_winner(matrix[x][y])=='x')
 			{
-				current->val=1;
-				return 1;
+				main_matrix[x][y]='x';
+				if(check_win_main(main_matrix))
+					{current->val=1;
+					return 1;
+					}
 			}
 		else
-			if (checkwin()=='o')
+			if (check_winner(matrix[x][y])=='o')
 				{
-					current->val=-1;
-					return -1;
+					main_matrix[x][y]=='o';
+					if(check_win_main(main_matrix))
+						{
+							current->val=-1;
+							return -1;
+						}
 				}
 			else
 				{
 					
-					empty=check_empty(matrix,a,b);
-					for(i=0;empty[i]i!=-1&&i<81;i++)
+					empty=check_empty(matrix,main_matrix,a,b);
+					for(i=0;empty[i]!=-1&&i<81;i++)
 						{
+							a=empty[i]/1000;
+							b=empty[i]/100 - a*10;
+							c=empty[i]/10 - a*100 - b*10;
+							d=empty[i]-a*1000 - b*100 - c*10;
 							memcpy(copy,matrix,sizeof(matrix));
 							current->child[i]=(struct Node *) malloc(sizeof(struct Node));
 							if(turn == 'x')
 								{
-								return minimax(current->child[i],'o',matrix);
+								copy[a][b][c][d]='x';
+								return minimax(current->child[i],'o',copy,main_martix,a,b);
 								}
 							else
 								{
-								return minimax(current->child[i],'x',matrix);
+								copy[a][b][c][d]='o';
+								return minimax(current->child[i],'x',copy,main_matrix,a,b);
 								}
 						}
+					if(turn=='x')
+						return maxmove(current);
+					else
+						return minmove(current);
 				}
 	}
 int main()
 	{
-		int i,j,k,l,x,y,matrix[3][3][3][3];
-		scanf("%d %d",&x,&y);
-		for(i=0;i<3;i++)
-			for(j=0;j<3;i++)
-				for(k=0;k<3;i++)
-					for(l=0;l<3;i++)
-                                        	scanf("%c",&matrix[i][j][k][l]);                  
+	char matrix[3][3][3][3];
+	char main_matrix[3][3];
 
-		printf("Trial");
-		return 0;
-	}
+	int iplay,jplay;
+	char temp;
+
+	scanf("%d %d",&iplay,&jplay);
+	scanf("%c",&temp);
+        int i,j,k,l,move;
+
+        for(i=0;i<3;i++)
+        {
+                for(j=0;j<3;j++)
+                {
+                        for(k=0;k<3;k++)
+                        {
+                                for(l=0;l<3;l++)
+                                {
+                                        scanf("%c",&matrix[i][k][j][l]);
+                                        if(matrix[i][k][j][l]=='X')matrix[i][k][j][l]='x';
+                                        else if(matrix[i][k][j][l]=='O')matrix[i][k][j][l]='o';
+                                        else if(matrix[i][k][j][l]=='-')matrix[i][k][j][l]='s';
+                                }
+                        }
+                        scanf("%c",&temp);
+                }
+        }
+
+        for (i=0;i<3;i++)
+        {
+                for(j=0;j<3;j++)
+                {
+                        main_matrix[i][j]=check_winner(matrix[i][j]);
+                }
+        }
+	minimax();
+
+        return 0;
+
+	}	
